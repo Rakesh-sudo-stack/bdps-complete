8ab4389104a42402c182e1653dd8fb20969246de
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2023-05-04 16:28:51 +0530

    made search bar active

--- ui/src/components/admin-pages/AdminNav.jsx
+++ ui/src/components/admin-pages/AdminNav.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const adminNav = (props) => {
+const AdminNav = (props) => {
+    const [searchInput, setSearchInput] = useState('');
     const showMenu = () => {
         document.querySelector('aside').classList.add('active');
     }
@@ -11,10 +13,63 @@ const adminNav = (props) => {
         document.querySelector('.menu-btn').style.display = 'none';
     }
     const hideSearchBar = () => {
+        
+        const highlightedElements = document.querySelectorAll('span[class^="highlight-"]');
+        console.log(highlightedElements)
+        if(highlightedElements.length>0){
+            highlightedElements.forEach(element=>{
+                let parent = element.parentNode;
+                
+                const textNode = document.createTextNode(element.innerHTML);
+                parent.replaceChild(textNode, element);
+            })
+        }
+
         document.querySelector('.search-bar').style.display = 'none';
         document.querySelector('.right-side').style.display = 'flex';
         document.querySelector('.left-side').style.display = 'flex';
         document.querySelector('.menu-btn').style.display = 'flex';
+    }
+    const searchAcrossPage = () => {
+        const highlightedElements = document.querySelectorAll('span[class^="highlight-"]');
+        console.log(highlightedElements)
+        if(highlightedElements.length>0){
+            highlightedElements.forEach(element=>{
+                let parent = element.parentNode;
+                
+                const textNode = document.createTextNode(element.innerHTML);
+                parent.replaceChild(textNode, element);
+            })
+        }
+        const searchTerm = searchInput;
+        const regex = new RegExp(searchTerm, 'gi');
+        let matchCount = 0;
+
+        const traverseNodes = (node) => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                const matches = node.nodeValue.match(regex);
+                if (matches !== null && matches.length > 0) {
+                    matchCount += matches.length;
+                    const span = document.createElement('span');
+                    span.classList.add(`highlight-${matchCount}`);
+                    const replacedText = node.nodeValue.replace(regex, `<span class="highlight-${matchCount}">$&</span>`);
+                    const newNode = document.createElement('span');
+                    newNode.innerHTML = replacedText;
+                    node.parentNode.replaceChild(newNode, node);
+                }
+            } else {
+                for (let i = 0; i < node.childNodes.length; i++) {
+                    traverseNodes(node.childNodes[i]);
+                }
+            }
+        };
+
+        traverseNodes(document.body);
+
+        if (matchCount > 0) {
+            const firstMatch = document.querySelector(`.highlight-1`);
+            firstMatch.scrollIntoView({ behavior: 'smooth' });
+        }
     }
     return (
         <div className="admin-nav">
@@ -35,13 +90,13 @@ const adminNav = (props) => {
             </div>
             <div className="search-bar">
                 <div className="container-box">
-                    <input type="text" placeholder="Search" />
+                    <input onChange={(e) => { setSearchInput(e.target.value) }} type="text" placeholder="Search" />
                     <div className="search-close-btn"><i className="fa-solid fa-xmark" onClick={hideSearchBar}></i></div>
-                    <button><i className="fa fa-search"></i></button>
+                    <button onClick={searchAcrossPage}><i className="fa fa-search"></i></button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default adminNav;
+export default AdminNav;
